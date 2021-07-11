9dc24b2074b036bd98588f7f2e3ff2c188410ccb
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-07-10 23:09:50 -0400

    execute deletion/select/conditional select/insertion with foreign key queries

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
 const Customer = require('./models/customer');
 const Order = require('./models/order');
 
-Customer.create({ 
-  name: "Joey Tribianni", 
-  email: 'jt@gmail.com'
-}, (err, customer) => {
-  if (err) { throw err }
-  console.log("customer is : ", customer)
-});
-
-// Customer.insertMany([ 
-//   { 
-//     name: "Joey Tribianni", 
-//     email: 'jt@gmail.com'
-//   },
-//   { 
-//     name: "Chandler Bing", 
-//     email: 'cb@gmail.com'
-//   },
-// ]).then((insertedCustomers) => {
-//   console.log(insertedCustomers);
-//   return Customer.deleteOne({ name: "Joey Tribianni" })
-// }).then(deletedCustomer => {
-//   console.log(deletedCustomer);
-// }).catch(e => { throw e });
-
-    // Delete all persons from the db.
-    // await Customer.query().delete();
-    // await Order.query().delete();
-  
-    // Insert one row to the database.
-    // const customer = await Customer.query().insert({
-    //   name: 'Rachel Green',
-    //   email: 'rg@gmail.com',
-    // });
-  
-    // Read all rows from the db.
-    // const customerRead = await Customer.query();
-    // console.log(customerRead);
-
-    // const order = await Customer.relatedQuery('order')
-    //                     .for(customer.id)
-    //                     .insert({ total: 55 });
-    // console.log(order);
-
-    // const orderTotal = await Order.query()
-    //                             .select('total')
-    //                             .where('customer_id', '=', customer.id)
-    //                             .orderBy('total');
-    
-    // console.log(orderTotal);
-  
+let customerId = null;
+Customer.insertMany([ 
+  { 
+    name: "Joey Tribianni", 
+    email: 'jt@gmail.com'
+  },
+  { 
+    name: "Chandler Bing", 
+    email: 'cb@gmail.com'
+  },
+]).then((insertedCustomers) => {
+  console.log("Inserted Customers are: ", insertedCustomers);
+  return Customer.deleteOne({ name: "Joey Tribianni" })
+}).then(deletedCustomer => {
+  console.log("Deleted: ", deletedCustomer);
+  return Customer.find();
+}).then((remainingCustomers) => {
+  console.log("Remaining Customer : ", remainingCustomers);
+  customerId = remainingCustomers[0]._id;
+  return Order.create({
+    customer_id: customerId,
+    total: 45
+  });
+}).then((order) => { 
+  console.log("Current Customer's Order is : ", order);
+  return Order.find({customer_id: customerId});
+}).then((selectedOrder) => {
+  console.log("Current Customer's Selected Order is : ", selectedOrder);
+})
+.catch(e => { throw e });
   

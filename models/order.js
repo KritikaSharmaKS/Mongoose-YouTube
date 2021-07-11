43849e189e9b7fa678bec0ec6db2c08405f8b4e6
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-07-10 23:07:27 -0400

    define association between Customer and Order collections

--- models/order.js
+++ models/order.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const connection  = require('../util/database');
+const Customer = require('./customer');
 
 const order = new mongoose.Schema({
   total: {
     type: Number,
     required: true
+  },
+  customer_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: Customer,
+    required: true,
+    index: true
   }
 });
 

adff420258d7f5f91316c5cbc435ca1aa211b837
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-07-11 18:59:46 -0400

    format documents

--- util/database.js
+++ util/database.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
-const connection = mongoose.createConnection('mongodb://localhost:27017/ks-store');
+const mongoose = require("mongoose");
+const connection = mongoose.createConnection(
+  "mongodb://localhost:27017/ks-store"
+);
 
 module.exports = connection;
